fed9c39cdbc3ce455c59aefc901344826ec7cdb4
jiangning.cui2 <jiangning.cui2@travelzen.com>
2014-04-14 15:37:52 +0800

    add by pfei

--- icloud-common/framework-logger/src/main/java/com/icloud/stock/dao/test/StockDaoTest.java
+++ icloud-common/framework-logger/src/main/java/com/icloud/stock/dao/test/StockDaoTest.java
@@ -1,22 +1,17 @@
 package com.icloud.stock.dao.test;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.icloud.stock.dao.IStockDao;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:spring/applicationContext-*" })
 public class StockDaoTest {
-	@Resource(name = "stockDao")
-	private IStockDao stockDao;
+	// @Resource(name = "")
 
 	@Test
 	public void getStockTest() {
-		stockDao.getList();
+		System.out.println("dasf");
 	}
 }
