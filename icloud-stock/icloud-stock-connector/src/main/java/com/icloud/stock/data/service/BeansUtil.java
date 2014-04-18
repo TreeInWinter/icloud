5e0aefd3175806cd0fa05a8df7bd3e121d3634e9
jiangning.cui2 <jiangning.cui2@travelzen.com>
2014-04-18 11:47:20 +0800

    af

---
+++ icloud-stock/icloud-stock-connector/src/main/java/com/icloud/stock/data/service/BeansUtil.java
@@ -0,0 +1,21 @@
+package com.icloud.stock.data.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.icloud.stock.service.IStockService;
+
+public class BeansUtil {
+	private static ApplicationContext app;
+
+	static {
+		app = new ClassPathXmlApplicationContext(
+				"spring/applicationContext-stock.xml");
+
+	}
+
+	public static IStockService getStockService() {
+		return (IStockService) app.getBean("stockService");
+	}
+
+}
