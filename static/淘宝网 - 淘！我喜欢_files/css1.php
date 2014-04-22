849938821c98d0a385ba4625f9e5d7cfe8f0c4c4
cuijiangningpc <jiangning.cui>
2014-04-23 01:24:34 +0800

    ok

---
+++ static/淘宝网 - 淘！我喜欢_files/css1.php
@@ -0,0 +1,4 @@
+var conveConfig = {
+
+conve: "http://g.tbcdn.cn/??tcc/tbr/1.0.24/tbr/common-min.css,tb/conve/0.0.10/common-min.css",phone: "http://g.tbcdn.cn/tcc/tbr/1.0.29/tbr/widget/phone/phone.css",game: "http://g.tbcdn.cn/tcc/tbr/1.0.26/tbr/widget/??game/new-service.css,game/game.css,game/game-all.css,qq/qq.css,card/card.css,yeyougame/yeyougame.css",trip: "http://g.tbcdn.cn/trip/marketing/0.0.9/trip-min.css",baoxian: "http://g.tbcdn.cn/fi/insure/conve/v2/common-min.css"
+}
