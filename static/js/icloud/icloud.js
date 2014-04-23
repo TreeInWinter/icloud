a544e330054f0ed0959594808a216c8de4686797
jiangning.cui2 <jiangning.cui2@travelzen.com>
2014-04-23 16:49:32 +0800

    特效

--- static/js/icloud/icloud.js
+++ static/js/icloud/icloud.js
@@ -1,16 +1,15 @@
-/** 采购商 公共 js 文件 */
-$(function() {
+/** 公共 js 文件 */
+function backToTopWind() {
     /*
     var offset = $(".main-body").eq(0).offset();
     var w = $(".main-body").eq(0).width();
     */
-
     var $backToTopTxt = "返回顶部", $backToTopEle = $('<a class="backToTop"></a>').appendTo($(".main-body"))
     .text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
             $("html, body").animate({ scrollTop: 0 }, 120);
     }), $backToTopFun = function() {
         var st = $(document).scrollTop(), winh = $(window).height();
-        (st > 800)? $backToTopEle.show(): $backToTopEle.hide();    
+        (st > 300)? $backToTopEle.show(): $backToTopEle.hide();    
         //IE6下的定位
         if (!window.XMLHttpRequest) {
             $backToTopEle.css("top", st + winh - 166);
@@ -29,149 +28,8 @@ $(function() {
     */
     $(function() { $backToTopFun(); });
 	
-    if(typeof($.datepicker) != "undefined"){
-        $.datepicker.regional[ "zh-CN" ];
+};
 
-        var dpSetting = {
-            css : {"z-index": 20000},
-			showDay:true,
-			numberOfMonths:[1,2],
-			minDate :new Date(),
-			firstDay:0,
-			showButtonPanel :true
-		};
-
-        var dpSettingUlt = {
-            css : {"z-index": 20000},
-			showDay:true,
-			numberOfMonths:[1,2],
-			firstDay:0,
-			showButtonPanel :true
-		};
-
-        var dpSettingShort = {
-            css : {"z-index": 20000},
-			numberOfMonths:[1,2],
-			firstDay:0,
-			showButtonPanel :true
-		};
-
-
-        var dpEle = $(".datepicker");
-
-        $.each(dpEle,function(){
-            var item = $(this);
-            if( item.hasClass("dpUlt") ){
-                item.datepicker(dpSettingUlt);
-            }else if(item.hasClass("dpShort")){
-                item.datepicker(dpSettingShort);
-            }else{
-                item.datepicker(dpSetting);
-            }
-        });
-    }
-		
-    if(typeof(kendo) != "undefined"){
-        kendo.init($("body"));
-    }
-
-    $("body").delegate(".toggle_trigger","click",function(){
-        var that = $(this),
-            t = that,
-            textFilter = t, 
-            temp,
-            i,j,
-            noslide,
-            tags = ['th','tr','td','table','tbody','thead','tfooter'];
-
-        if(that.attr("data-toggle")){
-            o = $.parseJSON(that.attr("data-toggle"));
-        }else{
-            o = $.parseJSON(that.attr("toggle"));
-        }
-
-        if(o.filer){
-            t = $(o.filter,that);
-        }
-
-        if(o.textFilter) {
-            textFilter = $(o.textFilter,that); 
-        }
-
-        temp = $(o.target).get(0);
-        noslide = false;
-
-        for(i = 0, j = tags.length; i < j; i++){
-            if(temp && (temp.tagName.toLowerCase() == tags[i])){
-                noslide = true;
-                break;
-            }
-        }
-
-        if(noslide){
-            if(o.wrapper){
-                $(o.target,that.closest(o.wrapper)).toggle();
-            }else{
-                $(o.target).toggle();
-            }
-
-        }else{
-            if(o.wrapper){
-                $(o.target,that.closest(o.wrapper)).slideToggle("normal");
-            }else{
-                $(o.target).slideToggle("normal");
-            }
-        }
-
-        /* 折叠 toggle_trigger */
-        if(o.toggleSelfClass){
-            if(typeof(o.toggleSelfClass) == "string"){
-                that.toggleClass(o.toggleSelfClass);
-            }else{
-                if(that.hasClass(o.toggleSelfClass[0])){
-                    that.removeClass(o.toggleSelfClass[0]).addClass(o.toggleSelfClass[1]);
-                }else{
-                    that.removeClass(o.toggleSelfClass[1]).addClass(o.toggleSelfClass[0]);
-                }
-            }
-        }
-
-        /* 折叠触发者 */
-        if(o.toggleClass){
-            if(typeof(o.toggleClass) == "string"){
-                t.toggleClass(o.toggleClass);
-            }else{
-                if(t.hasClass(o.toggleClass[0])){
-                    t.removeClass(o.toggleClass[0]).addClass(o.toggleClass[1]);
-                }else{
-                    t.removeClass(o.toggleClass[1]).addClass(o.toggleClass[0]);
-                }
-            }
-        }
-
-        /* 切换触发者文本 */
-        if(o.toggleText){
-            if(textFilter.html() == o.toggleText[0]){
-                textFilter.html(o.toggleText[1]);
-            }else{
-                textFilter.html(o.toggleText[0]);
-            }
-        }
-    });
-
-	
-	$(".upProcess i").each(function(index){
-		var cur = $(this);
-		var x = 0,y = 0;
-		if(cur.hasClass("hightlight")){
-			x = 16;
-		}else if(cur.hasClass("unable")){
-			x = 32;
-		}
-		
-		y = (parseInt(cur.html()) - 1) * 16;
-		
-		cur.css({"background-position":" -" + x + "px -" + y + "px"});
-	});
+$(document).ready(function(){
+    backToTopWind();
 });
-
