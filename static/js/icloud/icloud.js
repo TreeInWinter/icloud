d9dbcc0bd1c5ed833df4a1e72f1150997a081b5e
jiangning.cui2 <jiangning.cui2@travelzen.com>
2014-04-26 21:20:16 +0800

    增加翻页内容

--- static/js/icloud/icloud.js
+++ static/js/icloud/icloud.js
@@ -4,19 +4,24 @@ function backToTopWind() {
     var offset = $(".main-body").eq(0).offset();
     var w = $(".main-body").eq(0).width();
     */
-    var $backToTopTxt = "返回顶部", $backToTopEle = $('<a class="backToTop"></a>').appendTo($(".main-body"))
-    .text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
-            $("html, body").animate({ scrollTop: 0 }, 120);
-    }), $backToTopFun = function() {
-        var st = $(document).scrollTop(), winh = $(window).height();
-        (st > 250)? $backToTopEle.show(): $backToTopEle.hide();    
-        //IE6下的定位
-        if (!window.XMLHttpRequest) {
-            $backToTopEle.css("top", st + winh - 166);
-//            $advice.css("top", st + winh - 166);
-        }
-    };//$advice=$('<a class="advice"></a>').appendTo($("body"));
-    
+    var $backToTopTxt = "返回顶部",
+        $backToTopEle = $('<a class="backToTop"></a>').appendTo($(".main-body"))
+            .text($backToTopTxt).attr("title", $backToTopTxt).click(function () {
+                $("html, body").animate({
+                    scrollTop: 0
+                }, 120);
+            }),
+        $backToTopFun = function () {
+            var st = $(document).scrollTop(),
+                winh = $(window).height();
+            (st > 250) ? $backToTopEle.show() : $backToTopEle.hide();
+            //IE6下的定位
+            if (!window.XMLHttpRequest) {
+                $backToTopEle.css("top", st + winh - 166);
+                //            $advice.css("top", st + winh - 166);
+            }
+        }; //$advice=$('<a class="advice"></a>').appendTo($("body"));
+
     $(window).bind("scroll", $backToTopFun);
     /*
     $(window).bind("resize", function(){
@@ -26,10 +31,28 @@ function backToTopWind() {
         $advice.css({left:offset.left + w + 20});
     });
     */
-    $(function() { $backToTopFun(); });
-	
+    $(function () {
+        $backToTopFun();
+    });
+
 };
 
-$(document).ready(function(){
+$(function () {
+    $("#sidebar-menu > ul > li").hover(function () {
+        $(this).addClass("selected");
+        $(".menu-panel", this).addClass("selected");
+        var bgImg = $(".menu-item", this).css("background-image");
+        bgImg = bgImg.replace(".", "-hover.");
+        $(".menu-item", this).css("background-image", bgImg);
+    }, function () {
+        $(this).removeClass("selected");
+        $(".menu-panel", this).removeClass("selected");
+        var bgImg = $(".menu-item", this).css("background-image");
+        bgImg = bgImg.replace("-hover.", ".");
+        $(".menu-item", this).css("background-image", bgImg);
+    });
+});
+
+$(document).ready(function () {
     backToTopWind();
 });
