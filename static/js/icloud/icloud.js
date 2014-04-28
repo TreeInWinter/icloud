1061b726725449b1cb63f0652177dbc8d87323c3
jiangning.cui2 <jiangning.cui2@travelzen.com>
2014-04-28 09:56:04 +0800

    修改backToTopwind

--- static/js/icloud/icloud.js
+++ static/js/icloud/icloud.js
@@ -37,22 +37,6 @@ function backToTopWind() {
 
 };
 
-$(function () {
-    $("#sidebar-menu > ul > li").hover(function () {
-        $(this).addClass("selected");
-        $(".menu-panel", this).addClass("selected");
-        var bgImg = $(".menu-item", this).css("background-image");
-        bgImg = bgImg.replace(".", "-hover.");
-        $(".menu-item", this).css("background-image", bgImg);
-    }, function () {
-        $(this).removeClass("selected");
-        $(".menu-panel", this).removeClass("selected");
-        var bgImg = $(".menu-item", this).css("background-image");
-        bgImg = bgImg.replace("-hover.", ".");
-        $(".menu-item", this).css("background-image", bgImg);
-    });
-});
-
 $(document).ready(function () {
     backToTopWind();
 });
