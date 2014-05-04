44e908d8f40122239d21e84543d732e096210c0c
jiangning.cui2 <jiangning.cui2@travelzen.com>
2014-05-04 20:28:39 +0800

    dasfa

--- icloud-front/icloud-stock-front/src/main/java/com/icloud/facade/action/ICloudWelcomeController.java
+++ icloud-front/icloud-stock-front/src/main/java/com/icloud/facade/action/ICloudWelcomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WelcomeController {
+public class ICloudWelcomeController {
 	@RequestMapping("/index")
 	public String helloWorld() {
 		/**
--- icloud-front/icloud-stock-front/src/main/webapp/WEB-INF/icloud/icloud-footer.ftl
+++ icloud-front/icloud-stock-front/src/main/webapp/WEB-INF/icloud/icloud-footer.ftl
@@ -7,7 +7,7 @@
         <a href="#">问题反馈</a>
     </center>
     <center>
-        Copyright &copy; 2014 必有网 All Rights Reserved.
+        Copyright &copy; 2014 必有网 All Rights Reserved. Designed by Okry.
     </center>
     <center class="copyright">
         提示：投资有风险，入行需谨慎。
